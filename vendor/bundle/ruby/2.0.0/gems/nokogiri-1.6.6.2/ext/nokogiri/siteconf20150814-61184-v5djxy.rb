c93cc01d918d7c8769eb9790b229b109fc35f56a
l08084 <l08084@shibaura-it.ac.jp>
2015-08-15 16:13:20 +0900

    Initial commit

---
+++ vendor/bundle/ruby/2.0.0/gems/nokogiri-1.6.6.2/ext/nokogiri/siteconf20150814-61184-v5djxy.rb
@@ -0,0 +1,6 @@
+require 'rbconfig'
+dest_path = "./.gem.20150814-61184-1un1ex8"
+RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
+RbConfig::CONFIG['sitearchdir'] = dest_path
+RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
+RbConfig::CONFIG['sitelibdir'] = dest_path

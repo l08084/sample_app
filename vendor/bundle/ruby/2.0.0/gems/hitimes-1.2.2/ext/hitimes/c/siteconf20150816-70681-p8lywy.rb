1445cb9d8c71ff26747730f2060f99d953c97371
l08084 <l08084@shibaura-it.ac.jp>
2015-08-21 17:51:31 +0900

    Add a full_title helper

---
+++ vendor/bundle/ruby/2.0.0/gems/hitimes-1.2.2/ext/hitimes/c/siteconf20150816-70681-p8lywy.rb
@@ -0,0 +1,6 @@
+require 'rbconfig'
+dest_path = "./.gem.20150816-70681-4e4hi6"
+RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
+RbConfig::CONFIG['sitearchdir'] = dest_path
+RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
+RbConfig::CONFIG['sitelibdir'] = dest_path
